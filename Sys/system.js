f6397eefb28b857fe936a2b0e6cdbc6d2226ab0e
Silmi <89022084+leandrMachado@users.noreply.github.com>
2022-03-14 16:37:42 +0000

    V 0.0.0

---
+++ Sys/system.js
@@ -0,0 +1,6 @@
+const btton = document.getElementById('menu-button')
+
+
+btton.addEventListener('click', ()=>{
+    document.getElementById('conteiner-menu').classList.toggle('active-conteiner-menu')
+})
