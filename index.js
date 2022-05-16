ba51efbbeef4721dfa0a35bf083dd9e16015c7b4
Silmi@L <89022084+leandrMachado@users.noreply.github.com>
2022-05-16 17:16:45 +0100

    Build 0.0.0

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-var express = require("express");
-var app = express();
+var express = require('express');
 var path = require("path");
+var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './views/index.html'))
 })
-
-app.get("/chat", function(req, res){
-    res.sendFile(path.join(__dirname, './public/chat.html'))
-});
-
-app.use(express.static(__dirname + '/components'));
-app.use('/images', express.static('images'));
-app.use('/styles', express.static('styles'));
 
 var midPort = app.listen(port, function () {
     console.log('Server listening on port ' + port);
-})
-
-// set up socket connection
-var io = require('socket.io').listen(midPort);
-
-io.sockets.on('connection', (socket)=>{
-    socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
-    
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    })
 })
